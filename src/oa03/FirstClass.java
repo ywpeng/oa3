20e54b7145bfd4dcbf1e67ac88180163711ec457
ywpeng <13776170+ywpeng@users.noreply.github.com>
2020-03-28 17:43:10 +0800

    Update FirstClass.java
    github update 03

--- src/oa03/FirstClass.java
+++ src/oa03/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello2"); 
+		System.out.println("git hub update 03");
 	}
 }
